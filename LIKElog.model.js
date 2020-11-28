c91ddbd0b8ddb4ab6bfdc21d99413bacc716ed5a
Anita Ambrkhed <aa.anita1990@gmail.com>
2020-11-28 12:52:55 +0530

    added models

--- LIKElog.model.js
+++ LIKElog.model.js
@@ -1,6 +1,10 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const LIKElog= new Schema({
 
-    Id: {type: String, required: true },
+    id: {type: String, required: true },
     userId: { type: String, required: true },
     entity_Id: { type: String, required: true },
     entity_type: { type: String, required: true },
@@ -13,9 +17,9 @@ const LIKElog= new Schema({
 const LIKElog1= mongoose.model('LIKElog', LIKElog);
 
 mongoose.connect("mongodb://127.0.0.1:27017/MyCalender", {useNewUrlParser: true});
-const NewLIKElog = new LIKElog1({Id,
-    userId,
-    entity_Id,
-    entity_type,
-    isDeleted})
+const NewLIKElog = new LIKElog1({id: '1234',
+    userId: 'abc123',
+    entity_Id: 'xyz_123',
+    entity_type: 'likelog',
+    isDeleted: false})
 NewLIKElog.save()
