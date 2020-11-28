e49901a96416bf7f48b4a58d3552d0fe1224c5bd
Anita Ambrkhed <aa.anita1990@gmail.com>
2020-11-28 13:02:37 +0530

    added models

--- Report.model.js
+++ Report.model.js
@@ -1,6 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const Report= new Schema({
 
-    Id: {type: String, required: true },
+    id: {type: String, required: true },
     reported_by: { type: String, required: true },
     description: { type: String, required: true },
     entity_Id: { type: String, required: true },
@@ -14,10 +17,10 @@ const Report= new Schema({
 const Report1= mongoose.model('Report', Report);
 
 mongoose.connect("mongodb://127.0.0.1:27017/MyCalender", {useNewUrlParser: true});
-const NewReport = new Report1({Id,
-    reported_by,
-    description,
-    entity_Id,
-    entity_type,
-    isDeleted})
+const NewReport = new Report1({id: 'qwe123',
+    reported_by: 'asvds',
+    description: 'this is regarding repotredby',
+    entity_Id: 'admsnr124',
+    entity_type: 'qweyexsz',
+    isDeleted: false})
 NewReport.save()
