d665bf9b921c41363459be2f16c8f0d8fedf5491
Anita Ambrkhed <aa.anita1990@gmail.com>
2020-11-28 12:47:42 +0530

    added models

--- Like.model.js
+++ Like.model.js
@@ -1,6 +1,10 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const Like= new Schema({
 
-    Id: {type: String, required: true },
+    id: {type: String, required: true },
     count: { type: String, required: true },
     entity_Id: { type: String, required: true },
     entity_type: { type: String, required: true },
@@ -13,9 +17,9 @@ const Like= new Schema({
 const Like1= mongoose.model('Like', Like);
 
 mongoose.connect("mongodb://127.0.0.1:27017/MyCalender", {useNewUrlParser: true});
-const NewLike = new Like1({Id,
-    count,
-    entity_Id,
-    entity_type,
-    isDeleted})
+const NewLike = new Like1({id: '78945',
+    count: '2',
+    entity_Id: 'tio123',
+    entity_type: 'xyz',
+    isDeleted: false})
 NewLike.save()
