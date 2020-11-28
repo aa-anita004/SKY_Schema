f891d995f64d91982303f3c13e58374c79230def
Anita Ambrkhed <aa.anita1990@gmail.com>
2020-11-28 13:06:49 +0530

    added models

--- userinfo.model.js
+++ userinfo.model.js
@@ -1,9 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const Userinfo= new Schema({
 
-    Get_Profile: {type: String, required: true },
-    Get_User: { type: String, required: true },
-    Edit: { type: String, required: true },
-    Update_User: { type: String },
+    get_Profile: {type: String, required: true },
+    get_User: { type: String, required: true },
+    edit: { type: String, required: true },
+    update_User: { type: String },
     isDeleted: {type: Boolean}
 
 }, {
@@ -13,9 +16,9 @@ const Userinfo= new Schema({
 const Userinfo1= mongoose.model('Userinfo', Userinfo);
 
 mongoose.connect("mongodb://127.0.0.1:27017/MyCalender", {useNewUrlParser: true});
-const NewUserinfo = new Userinfo1({Get_Profile,
-    Get_User,
-    Edit,
-    Update_User, 
-    isDeleted})
+const NewUserinfo = new Userinfo1({get_Profile: 'aa.abdid004',
+    get_User: 'qjfome1123',
+    edit: 'djmsjf_adad',
+    update_User: 'djsjjs_ufdj', 
+    isDeleted: 'false'})
 NewUserinfo.save()
